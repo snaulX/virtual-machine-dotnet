c5db1e68af0a9e08e312d49a6e3ec25aba8d8555
Александр Гунгер <alexandergunger666@gmail.com>
2019-07-28 09:00:06 +0300

    Add ReturnExpression

---
+++ wolvm/ReturnExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wolvm
+{
+    public class ReturnExpression : VMExpression
+    {
+        public Value ParseExpression(params Value[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
