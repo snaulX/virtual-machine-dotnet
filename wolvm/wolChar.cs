c1f5729b18ca3da344efbb8a79eeb6565931c737
Александр Гунгер <alexandergunger666@gmail.com>
2019-08-04 10:40:45 +0300

    Add wolChar

---
+++ wolvm/wolChar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wolvm
+{
+    public class wolChar : Void
+    {
+        public wolChar() : base()
+        {
+            strtype = "char";
+            Enumerable.Range(Char.MinValue, Char.MaxValue).ToDictionary(x => x); //generate constants of wolChar
+        }
+    }
+}
