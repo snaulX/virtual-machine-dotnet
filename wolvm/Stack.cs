9558d002cad245a392e982435e7f273aa83f587d
snaulX <alexandergunger666@gmail.com>
2019-09-17 19:56:44 +0300

    Fix bug with assigment operator in 'var'

--- wolvm/Stack.cs
+++ wolvm/Stack.cs
@@ -1198,6 +1198,7 @@ namespace wolvm
                             while (current != '}')
                             {
                                 variable:
+                                current = stack_code[++position]; //skip '{'
                                 while (char.IsWhiteSpace(current)) //skip whitespaces
                                 {
                                     try
