1ef6c935ab3163222edb8a528361e35d28e1974b
snaulX <alexandergunger666@gmail.com>
2019-11-01 18:49:56 +0300

    Fill RunExpression and other adds

--- wolvm/expressions/RunExpression.cs
+++ wolvm/expressions/RunExpression.cs
@@ -8,6 +8,10 @@ namespace wolvm.expressions
     {
         public Value ParseExpression(params Value[] args)
         {
+            //for (int i = 0; i < args.Length; i++)
+            //{
+                ((wolBlock)args[0].type).Run();
+            //}
             return Value.VoidValue;
         }
     }
